39a184d640a10dd5b420ab4c61619bff2cf87a59
Amelia Agustin <nurindahameliaagustin@gmail.com>
2026-02-05 01:09:15 +0700

    psikolog user-side (psikologlist, psikologdetail) & psikolog admin-side (psikologmanagement)

---
+++ app/psikologdetail/page.tsx
@@ -0,0 +1,205 @@
+'use client'
+import { useState, useRef } from "react"
+import {BadgeCheck, GraduationCap, Stethoscope, Calendar, ChevronLeft, ChevronRight} from "lucide-react";
+import Navbar from "@/components/common/navbar";
+
+const scrollAmount = 150;
+
+export default function PsikologDetail(){
+
+    const psikolog= {
+        name: "Andi Zainuddin Japri, M.Psi, Psikolog",
+        photo: "/assets/psikologProfile/psikolog-Andi.png",
+        spesialis: "Psikologi Klinis",
+        nomorIzin: "12345678",
+        pendidikan: [
+            "S1 Psikologi Universitas Muhammadiyah Malang",
+            "Magister Psikologi Profesi Universitas Muhammadiyah Malang (2022)",
+        ],
+        description: "merupakan Praktisi Psikolog dengan Peminatan Klinis lulusan Magister Psikologi Profesi Universitas Muhammadiyah Malang yang ahli dalam melakukan asesmen psikologis individu dan kelompok, serta berpengalaman dalam memberikan layanan konseling dan Psikoterapi. Dengan pendekatan ilmiah dan empatik, saya berkomitmen membantu individu menemukan makna dan keseimbangan dalam menghadapi berbagai tantangan psikologis.",
+    }
+
+    const jadwalPraktik = [
+        {
+            day: "Senin",
+            date: "12 Jan",
+            times: ["09.00 WIB", "13.00 WIB", "13.00 WIB", "13.00 WIB", "13.00 WIB"],
+        },
+        {
+            day: "Selasa",
+            date: "13 Jan",
+            times: ["10.00 WIB"],
+        },
+        {
+            day: "Rabu",
+            date: "14 Jan",
+            times: ["13.00 WIB"],
+        },
+        {
+            day: "Kamis",
+            date: "15 Jan",
+            times: ["13.00 WIB"],
+        },
+        {
+            day: "Jumat",
+            date: "16 Jan",
+            times: ["13.00 WIB"],
+        },
+        {
+            day: "Sabtu",
+            date: "17 Jan",
+            times: ["14.00 WIB"],
+        },
+    ]
+
+    const [activeDay, setActiveDay] = useState(0);
+
+    const keahlian = [
+        "Assessment Psikologi",
+        "Konseling Psikologi",
+        "Intervensi Psikologi",
+        "Psikoterapi",
+    ];
+
+    const pengalamanKasus = [
+        "Permasalahan Komunikasi",
+        "Permasalahan Harga Diri",
+        "Depresi",
+        "Kecemasan",
+    ];
+
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const scrollLeft = () => {
+        scrollRef.current?.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    };
+
+    const scrollRight = () => {
+        scrollRef.current?.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    };
+
+    return(
+        <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+            <Navbar/>
+            <h1 className="text-center text-4xl font-bold text-blue-950 mt-16">Our Psikolog</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl sm:border bg-white sm:border-gray-200 sm:rounded-2xl sm:shadow-lg sm:p-4 my-8 sm:mx-8">
+                {/*Left */}
+                <div className="p-6 md:p-8 space-y-6">
+                    <div className="flex flex-col lg:flex-row gap-2 lg:gap-6">
+                        <img src={psikolog.photo} alt="Psikolog" className="w-28 h-28 rounded-full object-cover mx-auto md:mx-0"/>
+                        <div className="space-y-3 pt-4">
+                            <h2 className="text-lg font-bold text-blue-950 text-center md:text-start">
+                                {psikolog.name}
+                            </h2>
+                            <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
+                                <div className="flex gap-2">
+                                    <Stethoscope className="w-5 h-5 text-blue-950 mt-1"/>
+                                    <div>
+                                        <p className="text-sm font-semibold text-blue-950">
+                                            Spesialis
+                                        </p>
+                                        <p className="text-sm text-gray-600">
+                                            {psikolog.spesialis}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="flex gap-2">
+                                    <BadgeCheck className="w-5 h-h text-blue-950 mt-1"/>
+                                    <div>
+                                        <p className="text-sm font-semibold text-blue-950">
+                                            Nomor Izin Praktik
+                                        </p>
+                                        <p className="text-sm text-gray-600">
+                                            {psikolog.nomorIzin}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex gap-2">
+                            <GraduationCap className="w-5 h-5 text-blue-950"/>
+                            <p className="text-sm font-semibold text-blue-950 mb-2">Pendidikan</p>
+                        </div>
+                        <ul className="space-y-1 pl-6 text-sm text-gray-600">
+                            {psikolog.pendidikan.map((item, index) => (
+                                <li key={index}>• {item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <p className="text-sm text-gray-600 leading-relaxed">{psikolog.description}</p>
+                    
+                </div>
+
+                {/*Right*/}
+                <div className="p-4 md:p-6">
+                    <h3 className="text-lg font-semibold text-blue-950 mb-4">Jadwal Praktik</h3>
+
+                    <div className="flex gap-4">
+                        <div className="grid grid-cols-4 xl:grid-cols-7 gap-3 md:gap-4">
+                            {jadwalPraktik.map((item, index) => (
+                                <button 
+                                key={index} 
+                                onClick={() => setActiveDay(index)}
+                                className={`border rounded-md text-center px-2 py-1 cursor-pointer hover:bg-blue-100 transition
+                                ${
+                                    activeDay === index ? "bg-blue-100 border-blue-950" : "border-gray-300"
+                                }`}>
+                                        <p className="text-sm font-semibold text-blue-950">
+                                            {item.day}
+                                        </p>
+                                        <p className="text-xs text-gray-500">
+                                            {item.date}
+                                        </p>
+                                </button>
+                            ))}
+                            <div className="border border-blue-950 rounded-md grid items-center justify-center px-2">
+                                <Calendar className="w-5 h-5 text-blue-950"/>
+                                <p className="text-xs font-semibold text-blue-950">Jan</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap gap-3 mt-6">
+                        {jadwalPraktik[activeDay].times.map((time, index) => (
+                            <div
+                            key={index}
+                            className="border border-blue-950 rounded-md cursor-pointer hover:bg-blue-50 px-4 py-1 text-sm text-blue-950 font-semibold"
+                            >
+                            {time}
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="mt-6">
+                        <h3 className="text-lg font-semibold text-blue-950 mb-4">Keahlian</h3>
+                        <div className="flex flex-wrap gap-2 mb-6">
+                            {keahlian.map((item, index) => (
+                                <span
+                                key={index}
+                                className="border border-blue-950 rounded-full px-3 py-1 text-sm text-blue-950"
+                                >
+                                    {item}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="mt-6">
+                        <h3 className="font-semibold text-blue-950 mb-6">Pengalaman</h3>
+                        <div className="flex flex-wrap gap-2">
+                            {pengalamanKasus.map((item, index) => (
+                                <span
+                                key={index}
+                                className="border border-blue-950 rounded-full px-3 py-1 text-sm text-blue-950">
+                                    {item}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ components/features/admin/psikologManagement/psikolgCard.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { Trash2, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+import type { PsychologistData } from "@/components/features/admin/psikologManagement/psikologForm";
+
+type Psikolog = PsychologistData & {
+    id: string;
+};
+
+type PsikologCardProps = {
+    psikolog: Psikolog;
+    onDelete: (id: string) => void;
+    onEdit: (psikolog: Psikolog) => void;
+};
+
+export default function PsikologCard({
+    psikolog,
+    onDelete,
+    onEdit,
+    }: PsikologCardProps) {
+    const router = useRouter();
+    const MAX_NAME_LENGTH = 50; 
+    const shouldTruncateName = psikolog.name.length > MAX_NAME_LENGTH;
+
+    const MAX_SIPP_LENGTH = 20;
+    const shouldTruncateSIPP = psikolog.licenseNumber.length > MAX_SIPP_LENGTH;
+
+    return (
+        <div className="min-h-22 relative border border-gray-300 rounded-xl py-2 px-4 bg-white shadow w-full max-w-sm hover:bg-gray-100 cursor-pointer">
+            <div className="flex gap-2 pr-14">
+                <div className="shrink-0">
+                    {psikolog.photo ? (
+                        <img
+                        src={psikolog.photo}
+                        alt="Psikolog"
+                        className="rounded-full w-18 h-18 object-cover"
+                        />
+                    ) : (
+                        <div className="rounded-full w-18 bg-gray-100 flex items-center justify-center">
+                            <User className="text-gray-400" size={32} />
+                        </div>
+                    )}
+                </div>
+
+                <div className="flex flex-col gap-1 flex-1 min-w-0 pt-4">
+                    <p className={`text-blue-950 text-sm font-medium truncate ${shouldTruncateName ? 'truncate' : ''}`}>
+                        {psikolog.name}
+                    </p>
+                    <p className={`text-gray-500 text-xs truncate${shouldTruncateSIPP ? 'truncate' : ''}`}>
+                        SIPP: <span className="font-medium">{psikolog.licenseNumber}</span>
+                    </p>
+                </div>
+            </div>
+            
+            <div className="absolute bottom-2 right-3 flex gap-2 items-center">
+                <button 
+                onClick={(e) => {
+                    e.stopPropagation();
+                    onEdit(psikolog);
+                }}
+                className="text-[#234463] text-sm font-semibold hover:text-blue-950 cursor-pointer">
+                Edit
+                </button>
+                <Trash2 size={18} className="cursor-pointer hover:text-red-600 text-[#234463]" 
+                onClick={(e) => {
+                    e.stopPropagation();
+                    onDelete(psikolog.id);
+                }}
+                />
+            </div>
+
+        </div>
+    );
+}
